d6b84c4562f42ce6944b2a5d9d64b93fa7dbf6e0
amz <ameenmuhammed3456789@gmail.com>
2026-01-13 10:44:28 +0530

    first

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import *
+
+admin.site.register(expert_table)
+admin.site.register(user_table)
+admin.site.register(company_table)
+admin.site.register(feedback)
+admin.site.register(company_review)
+admin.site.register(complaint)
+admin.site.register(stock_table)
+admin.site.register(StockDailyHistory)
+admin.site.register(tips)
+admin.site.register(doubt)
+admin.site.register(PasswordResetOTP)
